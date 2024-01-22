430b8798a9dfc53ae2ea3cda3d6b7c81f2f635a3
Harsh ranjan <hr062774@gmail.com>
2024-01-22 08:10:09 +0530

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about" >
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me Sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
+            <div className={styles.aboutItemText}>
+                <h3>Frontend Developer</h3>
+                <p>
+                    I'm a Frontend Developer with experience in building responsive and optimised sites
+                </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="server Icon" />
+            <div className={styles.aboutItemText}>
+                <h3>Backend Developer</h3>
+                <p>
+                    I have experience developing fast and optimised back-end system and APIs
+                </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="UI Icon" />
+            <div className={styles.aboutItemText}>
+                <h3>UI Designer</h3>
+                <p>
+                    I have designed multiple landing pages and have created design systems as well
+                </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Harsh</h1>
+        <p className={styles.description}>
+          I'm a Software Engineer skilled in financial algorithms, adept at both
+          frontend and backend development.
+        </p>
+        <a href="mailto:hr062774@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero Image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
